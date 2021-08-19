1f3872335fc0cda9b797c0f0c3c68640ff21290e
hapo-tunght <tung.ht@haposoft.com>
2021-08-19 14:34:09 +0700

    create feedback slider

---
+++ js/style.js
@@ -0,0 +1,35 @@
+$('.feedback-slider-comment').slick({
+  draggable: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 1000,
+  prevArrow: '<button class="slick-arrow prev-arrow"><i class="fas fa-chevron-circle-left"></i></button>',
+  nextArrow: '<button class="slick-arrow next-arrow"><i class="fas fa-chevron-circle-right"></i></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
