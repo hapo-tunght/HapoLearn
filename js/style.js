deb85d816c9b55902776bf0130b6f90553e5f9e4
hapo-tunght <tung.ht@haposoft.com>
2021-08-23 17:44:59 +0700

    add plugin messenger chat

--- js/style.js
+++ js/style.js
@@ -40,4 +40,23 @@ $(document).ready(function () {
   $(".nav-tabs a").click(function () {
     $(this).tab('show');
   });
+
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "108828021517439");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  window.fbAsyncInit = function () {
+    FB.init({
+      xfbml: true,
+      version: 'v11.0'
+    });
+  };
+
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 });
